5ae08c85af57ce53c3d58917172519c01806d1f3
cago <vl4d2007ivan@mail.ru>
2024-02-11 16:42:17 +0300

    hello

---
+++ func.py
@@ -0,0 +1,6 @@
+def hel(name):
+    return f"Hello {name}"
+
+def sal(ph, h):
+    k=2
+    return ph*h*k
---
+++ test.py
@@ -0,0 +1,7 @@
+from func import sal, hel
+
+assert hel("Mike") == "Hello Mike"
+assert hel("Jo") == "Hello Jo"
+
+assert sal(2, 2) == 8
+assert sal(3, 3) == 18
